1d7b4ad2f6b5b71abde2da0e56dbe32edc8ad1f5
王證樺 <91170653+vaxco@users.noreply.github.com>
2022-02-24 17:28:00 +0800

    Add files via upload

---
+++ week01/week01-2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+///    int a;
+///    printf("請輸入一個數字: ");
+///    scanf("%d",&a);
+///    printf("你讀到了%d\n",a);
+
+    char c;
+    printf("請輸入一對字: ");
+    scanf("%c",&c);
+    printf("你讀到了 %c\n", c);
+
+    return 0;
+}
+
---
+++ week01/week01-5.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+char line[2000];
+
+int main()
+{
+
+	int sum=0;
+	while (scanf("%s",line)==1){
+		///for(int k=0;k<4;k++){
+
+		///scanf("%s", line);
+		int N= strlen(line);
+		int ans=0;
+		for(int i=0;i<N;i++){
+			if(line[i]=='2' ) ans++;
+		}
+		printf("%d\n",ans);
+		sum+=ans;
+	}
+
+	printf("Total: %d\n",sum);
+	return 0;
+
+
+
+}
